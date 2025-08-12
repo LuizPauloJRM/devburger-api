2501b79b09f4edfc5178db345825be5b4b47c7b8
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2025-08-12 20:56:43 -0300

    Criamos migrations tabela no banco model para fazer conexão ao banco para fazer interface e rota para criar algum registro

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,25 @@
 
 import { Router } from 'express'
+import { v4 } from 'uuid'
+
+
+import User from './app/models/User';
 
 const routes = new Router()
 
 //configuração de rota 
-routes.get('/', (req, res) => {
-    return res.status(200).json({ message: 'API is running' })
-})
+routes.get('/', async (req, res) => {
+
+    //return res.status(200).json({ message: 'API is running' })
+    const user = await User.create({
+        id: v4(),
+        name: 'Luiz Paulo',
+        email: 'luiz@eemail.com',
+        password_hash: '123456',
+    });
+
+    return res.status(201).json(user);
+});
+
 
 export default routes;
