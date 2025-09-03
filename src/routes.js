98c06c769275054cd9b18c9a7977e59596da501c
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2025-09-02 21:20:25 -0300

    Controller de produtos para cadastrar produtos

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,14 @@
 import { Router } from 'express'
 import UserController from './app/controllers/UserController.js'
 import SessionController from './app/controllers/SessionController.js'
+import ProductController from './app/controllers/ProductController.js'
 
 const routes = new Router()
 
 //configuração de rota 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
-
+routes.post('/products', ProductController.store);
 //return res.status(200).json({ message: 'API is running' })
 
 
